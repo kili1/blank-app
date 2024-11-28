64abbfa741053304eba86215826d1a993bb5f4b8
Kimoon Kim <17721944+kili1@users.noreply.github.com>
2024-11-28 12:18:24 +0000

    Getting Started

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import google.generativeai as genai
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+genai.configure(api_key="AIzaSyA262NFuT3JAtvLAdL6UfVTKPEGOtEr3cc")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def generate_response(input_text):
+  response = model.generate_content(input_text)
+  st.write(response)
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+    generate_response(text)
